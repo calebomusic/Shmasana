3af961213858ecfd855bd54f127e31752d2b3502
calebomusic <calebomusic@gmail.com>
2016-12-06 11:25:29 -0800

    Instantiate new prod build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,7 +2,7 @@ const webpack = require("webpack");
 
 module.exports = {
   context: __dirname,
-  entry: "./frontend/shmashana.jsx",
+  entry: "./frontend/shmasana.jsx",
   output: {
     path: "./app/assets/javascripts",
     filename: "bundle.js"
