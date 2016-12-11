4358fafd62b477ec558d69f2fd3a7c0af2e5aeb6
calebomusic <calebomusic@gmail.com>
2016-12-11 15:05:37 -0800

    Add comment seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -70,21 +70,31 @@ p3 = Project.create(name: 'Rebuild', workspace_id: w1.id)
 p4 = Project.create(name: 'Raise Awareness', workspace_id: w1.id)
 p4 = Project.create(name: 'Shift Paradigms', workspace_id: w1.id)
 
-# Tasks w/o project
+# Add comments
+
+Comment.create(task_id: task2.id, author_id: u0.id, body: "Let's find the best astronauts!" )
+Comment.create(task_id: task1.id, author_id: u0.id, body: "Let's have a lot of color!" )
+Comment.create(task_id: task1.id, author_id: u2.id, body: "On it as soon as I find the rover.")
+Comment.create(task_id: task1.id, author_id: u3.id, body: "?" )
+Comment.create(task_id: task4.id, author_id: u1.id , body: "Can't wait for another training session!")
+Comment.create(task_id: task5.id, author_id: u0.id , body: "Just did it")
+Comment.create(task_id: task6.id, author_id: u1.id, body: "Can't wait for another discussion.")
+Comment.create(task_id: task6.id, author_id: u2.id , body: "This issue is very important!" )
+Comment.create(task_id: task7.id, author_id: u3.id , body: "?" )
 
 # Guest user Tasks:
 
-task = Task.create(title: 'Design Flag', workspace_id: w1.id, author_id: u0.id,
+task1 = Task.create(title: 'Design Flag', workspace_id: w1.id, author_id: u0.id,
   description: "Design flag to place on Mars!", assignee_id: u2.id )
-task = Task.create(title: 'Recruit team of astronauts', workspace_id: w1.id, author_id: u0.id,
+task2 = Task.create(title: 'Recruit team of astronauts', workspace_id: w1.id, author_id: u0.id,
   description: "Junior astronauts accepted", assignee_id: u3.id )
-task = Task.create(title: 'Purchase appopriate spaceship and accessories',
+task3 = Task.create(title: 'Purchase appopriate spaceship and accessories',
  description: 'See also: spaceship project', workspace_id: w1.id, author_id: u0.id, completed: true, completed_at: time_rand, assignee_id: u2.id )
-task = Task.create(title: 'Organize anti-gravity training sesssion', workspace_id: w1.id, author_id: u0.id, assignee_id: u1.id )
-task = Task.create(title: 'Just do it', workspace_id: w1.id, author_id: u0.id, completed: true, completed_at: time_rand )
-task = Task.create(title: 'Organize water discussion', workspace_id: w1.id, author_id: u0.id,
+task4 = Task.create(title: 'Organize anti-gravity training sesssion', workspace_id: w1.id, author_id: u0.id, assignee_id: u1.id )
+task5 = Task.create(title: 'Just do it', workspace_id: w1.id, author_id: u0.id, completed: true, completed_at: time_rand )
+task6 = Task.create(title: 'Organize water discussion', workspace_id: w1.id, author_id: u0.id,
   description: "Discuss the possibility that there is no water", assignee_id: u1.id )
-task = Task.create(title: 'Blockchain video production machine learning', workspace_id: w1.id, author_id: u0.id,
+task7 = Task.create(title: 'Blockchain video production machine learning', workspace_id: w1.id, author_id: u0.id,
   description: "Infinity differentiates into subtle potentiality")
 
 task = Task.create(title: 'Notify media of launch', workspace_id: w1.id, author_id: u0.id,
@@ -93,7 +103,7 @@ task = Task.create(title: 'Run safety check of spaceship', workspace_id: w1.id, 
   description: "Saftey first", assignee_id: u3.id )
 task = Task.create(title: '', workspace_id: w1.id, author_id: u0.id, completed: true, completed_at: time_rand, assignee_id: u2.id )
 task = Task.create(title: 'Occupy mMars', workspace_id: w1.id, author_id: u0.id, assignee_id: u1.id )
-task = Task.create(title: 'Just do it', workspace_id: w1.id, author_id: u0.id, completed: true, completed_at: time_rand )
+task8 = Task.create(title: 'Just do it', workspace_id: w1.id, author_id: u0.id, completed: true, completed_at: time_rand )
 task = Task.create(title: 'Just do it', workspace_id: w1.id, author_id: u0.id,
   description: "Infinity differentiates into subtle potentiality", assignee_id: u1.id )
 
@@ -111,21 +121,32 @@ task = Task.create(title: 'Prepare for meeting other lifeforms', workspace_id: w
 task = Task.create(title: 'Buy spacebooks', workspace_id: w1.id, author_id: u1.id,
   description: "Kurt Vonnegut!")
 
+
+  Comment.create(task_id: task9.id, author_id: u0.id, body: "I favor SpaceX" )
+  Comment.create(task_id: task10.id, author_id: u2.id, body: "Don't forget about this!" )
+  Comment.create(task_id: task10.id, author_id: u1.id, body: "Completed!")
+  Comment.create(task_id: task12.id, author_id: u2.id, body: "Had a very pleasant conversation earlier" )
+  Comment.create(task_id: task13.id, author_id: u3.id , body: "?")
+  Comment.create(task_id: task13.id, author_id: u0.id , body: "This is important, Bob!")
+  Comment.create(task_id: task13.id, author_id: u1.id, body: "!")
+  Comment.create(task_id: task14.id, author_id: u2.id , body: "This issue is very important!" )
+  Comment.create(task_id: task9.id, author_id: u3.id , body: "?" )
+
 # Spaceship
-task = Task.create(title: 'Compare SpaceX and BlueOrigin', workspace_id: w1.id, author_id: u1.id, project_id: p0.id, assignee_id: u3.id)
-task = Task.create(title: 'Collect movie titles for the trip', workspace_id: w1.id, author_id: u1.id, project_id: p0.id,
+task9 = Task.create(title: 'Compare SpaceX and BlueOrigin', workspace_id: w1.id, author_id: u1.id, project_id: p0.id, assignee_id: u3.id)
+task10 = Task.create(title: 'Collect movie titles for the trip', workspace_id: w1.id, author_id: u1.id, project_id: p0.id,
   description: "Avoid space horror", completed: true, completed_at: time_rand, assignee_id: u1.id )
-task = Task.create(title: 'Call Elon', workspace_id: w1.id, author_id: u2.id, project_id: p0.id,
+task11 = Task.create(title: 'Call Elon', workspace_id: w1.id, author_id: u2.id, project_id: p0.id,
   description: "", completed: true, completed_at: time_rand, assignee_id: u2.id  )
-task = Task.create(title: 'Call Jeff', workspace_id: w1.id, author_id: u2.id, project_id: p0.id,
+task12 = Task.create(title: 'Call Jeff', workspace_id: w1.id, author_id: u2.id, project_id: p0.id,
   description: "", completed: true, completed_at: time_rand, assignee_id: u7.id )
-task = Task.create(title: 'Purchase space couch', workspace_id: w1.id, author_id: u4.id, project_id: p0.id,
+task13 = Task.create(title: 'Purchase space couch', workspace_id: w1.id, author_id: u4.id, project_id: p0.id,
   description: "The invisible reflects immortal force fields", assignee_id: u3.id)
-task = Task.create(title: 'Call recruiters', workspace_id: w1.id, author_id: u1.id, project_id: p0.id,
+task14 = Task.create(title: 'Call recruiters', workspace_id: w1.id, author_id: u1.id, project_id: p0.id,
   description: "The physical world creates intrinsic miracles", assignee_id: u2.id)
 task = Task.create(title: 'Energize team', workspace_id: w1.id, author_id: u1.id, project_id: p0.id,
   description: "Our consciousness is rooted in an abundance of balance", completed: true, completed_at: time_rand  )
-task = Task.create(title: 'Find best microwave', workspace_id: w1.id, author_id: u2.id, project_id: p0.id,
+task15 = Task.create(title: 'Find best microwave', workspace_id: w1.id, author_id: u2.id, project_id: p0.id,
   description: "Don't be cheap", completed: true, completed_at: time_rand, assignee_id: u1.id  )
 task = Task.create(title: 'Nap', workspace_id: w1.id, author_id: u2.id, project_id: p0.id,
   description: "", assignee_id: u3.id)
@@ -214,5 +235,3 @@ task = Task.create(title: 'Repeat', workspace_id: w2.id, author_id: u4.id,
   description: "The invisible reflects immortal force fields" )
 
 # Add completed at
-
-# Add comments
