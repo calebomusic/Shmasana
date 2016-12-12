abd81f62e1dae491dd80b846be69a61acba2ff37
calebomusic <calebomusic@gmail.com>
2016-12-11 18:30:25 -0800

    Remove icon till cloudinary resolved

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
+import Shepherd from 'tether-shepherd';
 
 import AllHeaders from './header/all_headers';
 import SideBarContainer from './sidebar/sidebar_container'
 import TaskListContainer from './tasks/task_list_container';
 import UserTour from './tour';
 
-const App = ({children, params}) => {
-  let tour;
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
 
-  return(
-  <div className='app'>
-    <SideBarContainer />
-    <div className='main-content'>
-      <AllHeaders />
-      <div className='tasks'>
-        <TaskListContainer />
-        {children}
-      </div>
-    </div>
-  </div>)
-};
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return(
+      <div className='app'>
+        <SideBarContainer />
+        <div className='main-content'>
+          <AllHeaders />
+          <div className='tasks'>
+            <TaskListContainer />
+            {this.props.children}
+          </div>
+        </div>
+      </div>)
+  }
+}
 
 export default App;
