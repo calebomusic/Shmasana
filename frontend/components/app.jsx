9e26f6c27411b1bcf56bd19b2d2850a979c9b3fb
calebomusic <calebomusic@gmail.com>
2016-12-15 16:31:51 -0800

    Revert "Remove icon till cloudinary resolved"
    This reverts commit abd81f62e1dae491dd80b846be69a61acba2ff37.

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,33 +1,24 @@
 import React from 'react';
-import Shepherd from 'tether-shepherd';
 
 import AllHeaders from './header/all_headers';
 import SideBarContainer from './sidebar/sidebar_container'
 import TaskListContainer from './tasks/task_list_container';
 import UserTour from './tour';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+const App = ({children, params}) => {
+  let tour;
 
-  componentDidMount() {
-    
-  }
-
-  render() {
-    return(
-      <div className='app'>
-        <SideBarContainer />
-        <div className='main-content'>
-          <AllHeaders />
-          <div className='tasks'>
-            <TaskListContainer />
-            {this.props.children}
-          </div>
-        </div>
-      </div>)
-  }
-}
+  return(
+  <div className='app'>
+    <SideBarContainer />
+    <div className='main-content'>
+      <AllHeaders />
+      <div className='tasks'>
+        <TaskListContainer />
+        {children}
+      </div>
+    </div>
+  </div>)
+};
 
 export default App;
